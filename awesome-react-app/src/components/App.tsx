4d020b9e9bb1d244920ef553e52b391bc96818c5
Anindya Mukherjee <anindya.sanchi@Anindyas-MacBook-Pro.local>
2024-10-20 16:43:52 +0530

    Tabs

--- awesome-react-app/src/components/App.tsx
+++ awesome-react-app/src/components/App.tsx
@@ -11,47 +11,15 @@ import './App.css';
 import { fetchData } from '../actions';
 import { Flex1, FlexColumn, FlexRow } from '../Flex';
 import { Button, Text } from '../BaseElements';
+import TabExample from './Tabs/TabExample';
 
 type ReduxProps = ConnectedProps<typeof connector>;
 type Props = ReduxProps;
 
-const Polling = lazy(() => import('./Polling'));
-const TextInput = lazy(() => import('./TextInput'));
-
-interface ImperativeProps {
-  isValid: () => boolean;
-  getValue: () => string;
-}
-
 const Fallback = () => <Text>Loading...</Text>;
 
 const App: React.FC<Props> = (props) => {
-  const [view, setView] = useState('');
-  return (
-    <Flex1 flexDirection="column">
-      <FlexColumn flex={1}>
-        <FlexRow justifyContent ="center" flex={1}>
-          <Button onClick={() => setView('POLLING')}>Show Polling</Button>
-          <Button onClick={() => setView('TEXT_INPUT')}>Show TextInoput</Button>
-        </FlexRow>
-      </FlexColumn>
-      <FlexColumn my={30}>
-        {view == 'POLLING' && (
-          <Suspense fallback={<Fallback />}>
-            <Polling
-              baseUrl={'https://jsonplaceholder.typicode.com/posts'}
-              frequency={3000}
-            />
-          </Suspense>
-        )}
-        {view == 'TEXT_INPUT' && (
-          <Suspense fallback={<Fallback />}>
-            <TextInput />
-          </Suspense>
-        )}
-      </FlexColumn>
-    </Flex1>
-  );
+  return <TabExample />;
 };
 
 const mapStateToProps = (state: any) => ({
